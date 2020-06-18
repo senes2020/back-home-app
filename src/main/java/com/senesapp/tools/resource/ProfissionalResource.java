4f88fb0ecc4b725bb966c2eaf03c03871ee23908
senes2020 <senessenai@gmail.com>
2020-06-18 08:50:27 -0300

    Adicionando home de Beneficiario com informações do Profissional

--- src/main/java/com/senesapp/tools/resource/ProfissionalResource.java
+++ src/main/java/com/senesapp/tools/resource/ProfissionalResource.java
@@ -7,6 +7,8 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -35,6 +37,12 @@ public class ProfissionalResource {
 	@GetMapping("/profissional/especializacoes/{especializacao}")
 	public List<Profissional> getProfissionaisPorEspecializacao(@PathVariable String especializacao){
 		return profissionalRepository.findProfissionalPorEspecializacao(especializacao);
+	}
+	
+	@PostMapping("/profissional/cadastrar")
+	public Profissional gravar(@RequestBody Profissional profissional) {
+		Profissional novoProfissional = profissionalRepository.save(profissional);
+		return novoProfissional;
 	}
 	
 }
